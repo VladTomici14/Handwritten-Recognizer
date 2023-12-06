cd833ef75155e6753ed94f25f99bffef209dd103
VladTomici14 <vladtomici@gmail.com>
2023-12-06 22:24:52 +0200

    Added the python backend flask infrastructure

--- django/uploads/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-
-from uploads.core import views
-
-url
